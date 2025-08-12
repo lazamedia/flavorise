9cbd4668c0a2ff9b9530281585e56abc9be122f1
lazuardi <lazamediamxt@gmail.com>
2025-08-12 14:06:29 +0700

    update

--- app/Http/Controllers/ShiftController.php
+++ app/Http/Controllers/ShiftController.php
@@ -168,6 +168,15 @@ class ShiftController extends Controller
             \Log::info('canAccessShift: User not authenticated');
             return false;
         }
+
+            // DEBUG: cek perbedaan ID dan tipe data
+    \Log::info('canAccessShift Debug', [
+        'auth_id' => $user->id,
+        'shift_user_id' => $shift->user_id,
+        'auth_id_type' => gettype($user->id),
+        'shift_user_id_type' => gettype($shift->user_id),
+        'equal' => $shift->user_id === $user->id ? 'YES' : 'NO'
+    ]);
         
         \Log::info('canAccessShift: User ID: ' . $user->id . ', Shift User ID: ' . $shift->user_id . ', Is Admin: ' . ($user->isAdmin() ? 'Yes' : 'No'));
         
